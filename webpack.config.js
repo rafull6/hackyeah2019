b34a396bf6e272603c09806c70f5b4ebe7df62db
Bartosz Wilk <bartek94@gmail.com>
2019-09-14 20:46:52 +0200

    Add react to app

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require("webpack");
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./app/index.jsx",
+  output: {
+    filename: "index.js",
+    publicPath: "/app/assets/",
+    path: path.resolve(__dirname, "www/js")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "www"),
+    hot: true,
+    historyApiFallback: true,
+    publicPath: "/js/",
+    open: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img",
+              publicPath: "/app/assets/img/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
